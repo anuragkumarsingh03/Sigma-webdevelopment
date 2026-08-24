363814eb42482d841aa97241953487ea8ba3b218
Anurag Kumar Singh <239380702+anuragkumarsingh03@users.noreply.github.com>
2026-08-24 12:20:14 +0530

    commit

--- video 57/index.js
+++ video 57/index.js
@@ -1,11 +1,13 @@
-console.log(" I am a tutorial on loops ");
+// console.log(" I am a tutorial on loops ");
 
-let a = 1;
+// let a = 1;
 
 // console.log(a)
 // console.log(a+1)
 // console.log(a+2)
 // console.log(a+3)
+
+
 // for(let i = 1 ; i<100;i++){
 //     console.log(i);
 // }
@@ -15,15 +17,16 @@ let a = 1;
 //     Role : "Programmer",
 //     Company : " CodewithHARRY",
 // }
-// for (const key in obj) {
-       
+// for (const key in obj) {    
 //     const element = obj[key];
 //     console.log(key, element); 
 // }
 
+
 // for (const c of "Anurag") {
 //     console.log(c)
 // }
+
 
 // let i = 5;
 // while (i<6) {
@@ -31,8 +34,29 @@ let a = 1;
 //     i++;  
 // }
 
-let i = 10; 
-do {
-    console.log(i);
-    i++; 
-} while (i<6);
+
+// let i = 10; 
+// do {
+//     console.log(i);
+//     i++; 
+// } while (i<6);
+
+
+// Quick Question
+
+let obj = {
+    Anurag : 98,
+    Fanishwar : 88,
+    Aditya : 89,
+}
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element)   
+// }
+// for (let student in obj) {
+//     console.log(student + " scored " + obj[student] + " marks ")
+// }
+
+for ( let student in obj){
+    console.log(student + " scored " + obj[ student ] + " marks ")
+}
---
+++ video 57/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let obj = {
+//     Name : "Anurag kumar singh",
+//     Role : "Programmer",
+//     Company : " CodewithHARRY",
+// }
+// for (const key in obj) {    
+//     const element = obj[key];
+//     console.log(key, element); 
+// }

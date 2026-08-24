2e81d775a2276ac92b855178f54b9a6b2c4a321e
Anurag Kumar Singh <239380702+anuragkumarsingh03@users.noreply.github.com>
2026-08-24 11:51:47 +0530

    commit

---
+++ video 57/index.js
@@ -0,0 +1,38 @@
+console.log(" I am a tutorial on loops ");
+
+let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+// for(let i = 1 ; i<100;i++){
+//     console.log(i);
+// }
+
+// let obj = {
+//     Name : "Anurag kumar singh",
+//     Role : "Programmer",
+//     Company : " CodewithHARRY",
+// }
+// for (const key in obj) {
+       
+//     const element = obj[key];
+//     console.log(key, element); 
+// }
+
+// for (const c of "Anurag") {
+//     console.log(c)
+// }
+
+// let i = 5;
+// while (i<6) {
+//     console.log(i);
+//     i++;  
+// }
+
+let i = 10; 
+do {
+    console.log(i);
+    i++; 
+} while (i<6);
---
+++ video 58/index.js
@@ -0,0 +1,40 @@
+// console.log("Hey Anurag you are a good person");
+// console.log("Hey Anurag you are a nice person");
+// console.log("Hey Anurag your tshirt is good");
+// console.log("Hey Anurag you are a absolute good person");
+// console.log("Hey Anurag you are a good person");
+
+
+// function nice(name) {
+//     console.log("Hey " + name + " you are nice");
+//     console.log("Hey " + name + " you are good");
+//     console.log("Hey " + name + " your tshirt is nice");
+//     console.log("Hey " + name + " you are styding good");
+// }
+// nice("Anurag");
+// nice("Fanishwar");
+
+
+function sum(a, b, c= 4) {
+    // console.log(a + b);
+    return  a + b + c;
+}
+ result1 = sum(4, 5);
+ result2 = sum(5, 5);
+ result3 = sum(8, 5);
+ result4 = sum(4, 13);
+
+ console.log("the sum of these number is" , result1)
+ console.log("the sum of these number is" , result2)
+ console.log("the sum of these number is" , result3)
+ console.log("the sum of these number is" , result4)
+
+const func1 = (x) => {
+    console.log("I am a arrow function", x)
+}
+func1(46);
+func1(78);
+func1(98);
+
+
+
